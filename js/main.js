7d561879dc5c762bacab7964160dbf81cc9d69a3
takpika <takpika000@gmail.com>
2020-11-10 12:55:07 +0800

    js

--- js/main.js
+++ js/main.js
@@ -18,9 +18,9 @@ window.onload = function(){
 
 function setlanguage(lang){
     var url = document.location.href;
-    if (lang == "en" && url == "https://takpika.github.io/NicoClip/"){
-        document.location.replace("/NicoClip/en/");
-    }else if (url == "https://takpika.github.io/NicoClip/en/"){
+    if (lang == "ja" || lang == "ja-jp" && url == "https://takpika.github.io/NicoClip/en/"){
         document.location.replace("/NicoClip/");
+    }else if (url == "https://takpika.github.io/NicoClip/"){
+        document.location.replace("/NicoClip/en/");
     }
 }
